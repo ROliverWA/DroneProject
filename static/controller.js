506d78a9653b8663afddec62d16ff994dcc2754b
Denny <dennylim92@aol.com>
2019-05-29 15:38:51 -0700

    removed unused functions

--- static/controller.js
+++ static/controller.js
@@ -8,13 +8,4 @@ module.exports = {
     res.render("index");
   },
 
-  // car: (req, res) => {
-  //   console.log('hit controller');
-  //   fetch('./cobra_sports_car/cobra_sports_car.glb')
-  //     .then(function(data) {
-  //       return res.blob(data);
-  //     })
-  //     .catch(err => res.json(err));
-  // }
-
 }
--- static/routes.js
+++ static/routes.js
@@ -5,7 +5,5 @@ module.exports = function(app) {
   console.log('hit routes');
 
   app.get('/', controller.index)
-  // app.get('/cobra_sports_car/cobra_sports_car.glb', controller.car)
-
 
 }
