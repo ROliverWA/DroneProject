23a29060941559b04a759cd35392bda680466206
Joe Linares <bwajal@gmail.com>
2019-05-29 10:33:59 -0700

    Added functions

--- public/src/app/app.component.ts
+++ public/src/app/app.component.ts
@@ -10,12 +10,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'public';
-  game_full: boolean = false;
-  count = 0;  
-  players: any = [];
-  teams_ready: boolean = false;
-  team1;
-  team2;
+  // game_full: boolean = false;
+  // count = 0;
+  // players: any = [];
+  // teams_ready: boolean = false;
+  // team1;
+  // team2;
 
 
 constructor(private socketService: SocketService) {
@@ -30,27 +30,27 @@ constructor(private socketService: SocketService) {
     }
 
 
-    logPlayer(roll,name, team) {  
-    PlayerManager.logPlayer(roll, name, team);
-    this.count ++;    
-    if (this.count === 4) {
-      this.game_full = true;
-      }
+    // logPlayer(roll,name, team) {
+    // PlayerManager.logPlayer(roll, name, team);
+    // this.count ++;
+    // if (this.count === 4) {
+    //   this.game_full = true;
+    //   }
 
-    }
+    // }
 
-    startGame() {
-    console.log("start game clicked");
-    this.players = PlayerManager.getPlayers();
-    this.players = PlayerManager.splitTeams(this.players);
-    this.teams_ready = true
-    console.log(this.players);
-    this.team1 = new PlayerManager.Team(this.players['1']);
-    this.team2 = new PlayerManager.Team(this.players['2']);
-    console.log(this.team1);
-    console.log(this.team2);
-    
-    }
+    // startGame() {
+    // console.log("start game clicked");
+    // this.players = PlayerManager.getPlayers();
+    // this.players = PlayerManager.splitTeams(this.players);
+    // this.teams_ready = true
+    // console.log(this.players);
+    // this.team1 = new PlayerManager.Team(this.players['1']);
+    // this.team2 = new PlayerManager.Team(this.players['2']);
+    // console.log(this.team1);
+    // console.log(this.team2);
+
+    // }
 
 
 
--- public/src/app/home/home.component.ts
+++ public/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as PlayerManager from "../PlayerManager";
 
 @Component({
   selector: 'app-home',
@@ -6,10 +7,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  game_full: boolean = false;
+  count = 0;
+  players: any = [];
+  teams_ready: boolean = false;
+  team1;
+  team2;
 
   constructor() { }
 
   ngOnInit() {
   }
+  logPlayer(roll,name, team) {
+    PlayerManager.logPlayer(roll, name, team);
+    this.count ++;
+    if (this.count === 4) {
+      this.game_full = true;
+      }
 
+    }
+
+    startGame() {
+    console.log("start game clicked");
+    this.players = PlayerManager.getPlayers();
+    this.players = PlayerManager.splitTeams(this.players);
+    this.teams_ready = true
+    console.log(this.players);
+    this.team1 = new PlayerManager.Team(this.players['1']);
+    this.team2 = new PlayerManager.Team(this.players['2']);
+    console.log(this.team1);
+    console.log(this.team2);
+
+    }
 }
