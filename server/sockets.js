2adc2b0b72c835f3e446efba33a2637f107d2bd3
Robert Oliver <ROliverWA@Outlook.com>
2019-05-31 10:32:53 -0700

    full controls done, waiting on integration to controller

--- server/sockets.js
+++ server/sockets.js
@@ -24,17 +24,101 @@ module.exports = (io, drone1, drone2) => {
         io.emit('roleselected', selected_roles);
         console.log('selected_roleszzzzz');
     });
+    socket.on('forward1', message => {
+        drone1
+        .after(100, function() {
+          this.front(".2");
+        })
+        .after(1700, function() {
+          this.stop();
+        });  
+        io.emit('roleselected', selected_roles);
+        console.log('selected_roleszzzzz');
+    });
+
+    socket.on('back1', message => {
+        drone1
+        .after(100, function() {
+          this.back(".2");
+        })
+        .after(1700, function() {
+          this.stop();
+        });
+        io.emit('roleselected', selected_roles);
+        console.log('selected_roleszzzzz');
+    });
     socket.on('left1', message => {
-        drone1.left(".2");       
+        drone1
+        .after(100, function() {
+          this.left(".2");
+        })
+        .after(1700, function() {
+          this.stop();
+        });    
         io.emit('roleselected', selected_roles);
         console.log('selected_roleszzzzz');
     });
 
     socket.on('right1', message => {
-        drone1.right(".2");     
+        drone1
+        .after(100, function() {
+          this.right(".2");
+        })
+        .after(1700, function() {
+          this.stop();
+        }); 
         io.emit('roleselected', selected_roles);
         console.log('selected_roleszzzzz');
     });
+
+    socket.on('up1', message => {
+        drone1
+        .after(100, function() {
+          this.up(".2");
+        })
+        .after(1700, function() {
+          this.stop();
+        });    
+        io.emit('roleselected', selected_roles);
+        console.log('selected_roleszzzzz');
+    });
+
+    socket.on('down1', message => {
+        drone1
+        .after(100, function() {
+          this.down(".2");
+        })
+        .after(1700, function() {
+          this.stop();
+        });    
+        io.emit('roleselected', selected_roles);
+        console.log('selected_roleszzzzz');
+    });
+
+    socket.on('cw1', message => {
+        drone1
+        .after(100, function() {
+          this.clockwise(".2");
+        })
+        .after(1700, function() {
+          this.stop();
+        });    
+        io.emit('roleselected', selected_roles);
+        console.log('selected_roleszzzzz');
+    });
+
+    socket.on('ccw1', message => {
+        drone1
+        .after(100, function() {
+          this.counterClockwise(".2");
+        })
+        .after(1700, function() {
+          this.stop();
+        });      
+        io.emit('roleselected', selected_roles);
+        console.log('selected_roleszzzzz');
+    });
+
     socket.on('start2', message => {
         drone2.takeoff();       
         io.emit('roleselected', selected_roles);
@@ -47,5 +131,103 @@ module.exports = (io, drone1, drone2) => {
         io.emit('roleselected', selected_roles);
         console.log('selected_roleszzzzz');
     });
-    }); 
+    
+
+    socket.on('left2', message => {
+        drone2
+        .after(100, function() {
+          this.left(".2");
+        })
+        .after(1700, function() {
+          this.stop();
+        });      
+        io.emit('roleselected', selected_roles);
+        console.log('selected_roleszzzzz');
+    });
+
+    socket.on('right2', message => {
+        drone2
+        .after(100, function() {
+          this.right(".2");
+        })
+        .after(1700, function() {
+          this.stop();
+        });   
+        io.emit('roleselected', selected_roles);
+        console.log('selected_roleszzzzz');
+    });
+
+    socket.on('forward2', message => {
+        console.log('forward2');
+        drone2
+    .after(100, function() {
+      this.front(".2");
+    })
+    .after(1700, function() {
+      this.stop();
+    });
+        io.emit('roleselected', selected_roles);
+        console.log('selected_roleszzzzz');
+    });
+
+    socket.on('back2', message => {
+        drone2
+        .after(100, function() {
+          this.back(".2");
+        })
+        .after(1700, function() {
+          this.stop();
+        });
+        io.emit('roleselected', selected_roles);
+        console.log('selected_roleszzzzz');
+    });
+
+    socket.on('up2', message => {
+        drone2
+        .after(100, function() {
+          this.up(".2");
+        })
+        .after(1700, function() {
+          this.stop();
+        });
+        io.emit('roleselected', selected_roles);
+        console.log('selected_roleszzzzz');
+    });
+
+    socket.on('down2', message => {
+        drone2
+        .after(100, function() {
+          this.down(".2");
+        })
+        .after(1700, function() {
+          this.stop();
+        });     
+        io.emit('roleselected', selected_roles);
+        console.log('selected_roleszzzzz');
+    });
+
+    socket.on('cw2', message => {
+        drone2
+        .after(100, function() {
+          this.clockwise(".2");
+        })
+        .after(1700, function() {
+          this.stop();
+        });      
+        io.emit('roleselected', selected_roles);
+        console.log('selected_roleszzzzz');
+    });
+
+    socket.on('ccw2', message => {
+        drone2
+        .after(100, function() {
+          this.counterClockwise(".2");
+        })
+        .after(1700, function() {
+          this.stop();
+        });       
+        io.emit('roleselected', selected_roles);
+        console.log('selected_roleszzzzz');
+    });
+}); 
 };
