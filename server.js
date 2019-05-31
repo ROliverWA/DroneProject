a531a0ec2e8c733feb177b9ba1c57003fc887590
Robert Oliver <ROliverWA@Outlook.com>
2019-05-30 22:11:34 -0700

    socket adjustment

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const http = require('http');
 const gameHost = http.Server(app);
 var arDrone = require('ar-drone');
-var selected_roles = 0;
+
 
 
 var droneOne = arDrone.createClient('192.168.1.2');
@@ -21,31 +21,6 @@ gameHost.listen(1337, () => console.log("Listening on port 1337"));
 const io = require('socket.io')(gameHost); 
 
 
-// droneOne.takeoff();
+require('./server/sockets')(io);
 
-// droneOne.after(5000, () => {
-//     droneOne.stop();
-//     droneOne.land();
-// });
-
-// droneTwo.takeoff();
-
-// droneTwo.after(5000, () => {
-//     droneTwo.stop();
-//     droneTwo.land();
-// });
-
-io.on('connection', socket => {    
-        console.log('got it'); 
-        socket.emit("connected");  
-    
-    socket.on('message', (message) => {
-        console.log("Message ", message);
-    });
-
-    socket.on('role_selected', message => {
-        selected_roles++;       
-        io.emit('roleselected', selected_roles);
-        console.log('selected_roles');
-    });
-    });   
+  
